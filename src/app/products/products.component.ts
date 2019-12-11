7a8372983ec3706818e193741258da071555d23e
Arindam Saha <arindam27.as@gmail.comm>
2019-12-11 18:47:52 +0530

    Text change

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -40,8 +40,8 @@ export class ProductsComponent implements OnInit {
   getProductImages(productName: string) {
     console.log('productName', productName);
     this.productImage = {
-      poster: this.baseUrl + 'poster-' + productName + '.png',
-      posterMob: this.baseUrl + 'poster-' + productName + '-mob.png',
+      poster: this.baseUrl + 'poster-' + productName + '.jpg',
+      posterMob: this.baseUrl + 'poster-' + productName + '-mob.jpg',
       productInfo: this.baseUrl + 'productInfo-' + productName + '.png',
       reference1: this.baseUrl + 'reference1-' + productName + '.png',
       reference2: this.baseUrl + 'reference2-' + productName + '.png',
@@ -50,11 +50,13 @@ export class ProductsComponent implements OnInit {
 
     this.productInfo = {
       header: (productName == 'shampoo') ? `Sesa Ayurvedic Shampoo` : `Sesa Ayurvedic Hair Oil`,
-      description: (productName == 'shampoo') ? `Sesa Ayurvedic Shampoo is an ayurvedic blend of 17 botanicals like Bhringraj, triphala, mehandi,
-        jati and yashtimadhu which cleanses and maintains the integrity of scalp and hair. It is the perfect solution for optimum hair nourishment and complete
-        care.` : `Sesa Ayurvedic is a distinctive combination of 18 nourishing herbs, 5 nourishing oils and milk all boiled together in
-          a special process called the 'Kshirpak Vidhi'. It consists of natural ingredients like Rasot, Neem beej, Chamelee, Tagar, Khas, Elva, essential oils
-            and Karan Beej. These natural and ayurvedic ingredients helps prevent infection, mental stress, dandruff & lice.`,
+      description: (productName == 'shampoo') ? `Sesa Ayurvedic Oil is a distinctive combination of 18 
+      nourishing herbs, 5 nourishing oils and milk all boiled together in a special process called the 
+      'Kshirpak Vidhi'. It consists of natural ingredients like Rasot, Neem beej, Chamelee, Tagar, Khas, 
+      Elva, essential oils and Karan Beej. These natural and ayurvedic 
+      ingredients helps prevent infection, mental stress, dandruff & lice.` : `Sesa Ayurvedic Shampoo is an ayurvedic blend of 17 botanicals like Bhringraj, triphala, 
+        mehandi, jati and yashtimadhu which cleanses and maintains the integrity of scalp and hair. 
+        It is the perfect solution for optimum hair nourishment and complete care.`,
       reference1: (productName == 'shampoo') ? `Provides clean and nourished hair` : `Stops hair fall`,
       reference2: (productName == 'shampoo') ? `Prevents hair fall` : `Prevents dandruff`,
       reference3: (productName == 'shampoo') ? `Protects from dandruff and infections` : `Promotes hair growth`,
